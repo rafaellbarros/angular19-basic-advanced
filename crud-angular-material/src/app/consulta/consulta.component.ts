5a1c6c8ab42393d655437a938ad86c6af86b7baa
Rafael Barros <rafaelbarros.df@gmail.com>
2026-07-29 11:20:59 -0300

    ✨ feat: update consulta component and prettier configuration

--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms'
+import { MatButton, MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, MatCardModule, MatIconModule, FlexLayoutModule, FormsModule, MatTableModule, MatButton],
   templateUrl: './consulta.component.html',
-  styleUrl: './consulta.component.scss'
+  styleUrl: './consulta.component.scss',
 })
-export class ConsultaComponent {
-
-}
+export class ConsultaComponent {}
